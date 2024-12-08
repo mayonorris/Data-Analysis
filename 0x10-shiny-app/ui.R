c0f0fc4111157fd513d5310f247950f01d463610
Mayo Takémsi Norris KADANGA <kadanganorris@gmail.com>
2024-12-08 01:24:06 +0100

    renderUI function in shiny

---
+++ 0x10-shiny-app/ui.R
@@ -0,0 +1,19 @@
+library(shiny)
+
+shinyUI(fluidPage(
+     titlePanel(h2("Submit button in shiny-Part II", align = "center")),
+     sidebarLayout(sidebarPanel(
+          selectInput("dataset", "select a dataset", choices = c("iris", "mtcars", "pressure")),
+          numericInput("frac", "Enter the proportion of the dataset that must be displayed. Range : 0-1: ", 
+                       value = 0.05, min = 0.01, max = 1),
+          submitButton("Update!"),
+          p("Click on the update button to update your choices")
+     ),
+          mainPanel(
+               h3(textOutput("dataname")),
+               verbatimTextOutput("structure"),
+               h4(textOutput("observation")),
+               tableOutput("view")
+                   ))
+)
+)
